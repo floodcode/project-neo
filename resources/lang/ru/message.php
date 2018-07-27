39b746fbe6c3a1b1ba334080bb85b1a88210f69f
floodcode <flood.code@gmail.com>
2018-07-27 15:02:04 +0000

    New translations message.php (Russian)

--- resources/lang/ru/message.php
+++ resources/lang/ru/message.php
@@ -3,6 +3,13 @@
 return [
 
     // Generic
-    'nothing-to-show' => 'Nothing to show'
+    'nothing-to-show' => 'Nothing to show',
+
+    'validation' => [
+        // News
+        'news-title-required' => 'Post title field is required',
+        'news-title-max' => 'Post title length should not exceed 255',
+        'news-message-required' => 'Post message field is required'
+    ]
 
 ];
