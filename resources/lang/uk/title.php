4de84441430ae0f121e847e198ff7982c42e5e2f
floodcode <flood.code@gmail.com>
2018-07-27 06:11:36 +0000

    New translations title.php (Ukrainian)

---
+++ resources/lang/uk/title.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    // Auth
+    'register' => 'Реєстрація',
+    'login' => 'Увійти',
+    'reset-password' => 'Скинути пароль',
+
+    // News
+    'news' => 'Новини',
+    'add-post' => 'Додати новину',
+    'edit-post' => 'Edit Post'
+
+];
