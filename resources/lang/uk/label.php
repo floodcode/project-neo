1999576bbe72bdd31ab813e138b4b2282caaa9f0
floodcode <flood.code@gmail.com>
2018-07-27 06:11:34 +0000

    New translations label.php (Ukrainian)

---
+++ resources/lang/uk/label.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    // Auth
+    'name' => 'Name',
+    'email-address' => 'E-Mail Address',
+    'password' => 'Password',
+    'confirm-password' => 'Confirm Password',
+    'remember-me' => 'Remember Me',
+
+    // News
+    'title' => 'Title',
+    'description' => 'Description'
+
+];
