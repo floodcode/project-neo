9bbc5d607204b1e0065546b1cb1fd619612c4883
floodcode <flood.code@gmail.com>
2018-07-31 20:55:26 +0000

    New translations message.php (Ukrainian)

--- resources/lang/uk/message.php
+++ resources/lang/uk/message.php
@@ -3,21 +3,21 @@
 return [
 
     // Generic messages
-    'nothing-to-show' => 'Nothing to show',
+    'nothing-to-show' => 'Пусто',
 
     // News
-    'delete-this-item?' => 'Are you sure you want to delete this item?',
+    'delete-this-item?' => 'Ви дійсно бажаєте видалити цей елемент?',
 
     'validation' => [
         // News
-        'news-title-required' => 'Post title field is required',
-        'news-title-max' => 'Post title length should not exceed 255',
-        'news-message-required' => 'Post message field is required'
+        'news-title-required' => 'Поле заголовок обов\'язкове',
+        'news-title-max' => 'Довжина заголовку не повинна перевищувати 255 символів',
+        'news-message-required' => 'Поле повідомлення обов\'язкове'
     ],
 
     // Errors
     'error' => [
-        'not-authorized' => 'You are not authorized to perform this action'
+        'not-authorized' => 'Ви не маєте прав на виконання цієї дії'
     ]
 
 ];
