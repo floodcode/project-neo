ccbdc96006ccef4a85e32b2b8847fa604fe8645e
floodcode <flood.code@gmail.com>
2018-07-31 20:56:57 +0000

    New translations title.php (Russian)

--- resources/lang/ru/title.php
+++ resources/lang/ru/title.php
@@ -3,14 +3,14 @@
 return [
 
     // Auth
-    'register' => 'Register',
-    'login' => 'Login',
-    'reset-password' => 'Reset Password',
+    'register' => 'Регистрация',
+    'login' => 'Войти',
+    'reset-password' => 'Восстановить пароль',
 
     // News
-    'news' => 'News',
-    'add-post' => 'Add New Post',
-    'edit-post' => 'Edit Post',
-    'delete-post' => 'Delete Post'
+    'news' => 'Новости',
+    'add-post' => 'Добавить новость',
+    'edit-post' => 'Редактировать пост',
+    'delete-post' => 'Удалить пост'
 
 ];
